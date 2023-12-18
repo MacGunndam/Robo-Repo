15c4b6838c005d08f7e616c48796a079fed1e73a
Ricardo G <ricardo.gonzalezjr@colorado.edu>
2023-12-18 09:49:45 -0700

    final working code for robot

--- reactive_motor_controller.py
+++ reactive_motor_controller.py
@@ -30,9 +30,9 @@ OFF = 0
 ################################
 # Feature Activation
 ################################
-FEATURE_STOP_SIGN = OFF
+FEATURE_STOP_SIGN = ON
 FEATURE_TELEMETRY = ON
-FEATURE_HW_MONITOR = OFF
+FEATURE_HW_MONITOR = ON
 FEATURE_BACKWARD_DRIVING = OFF
 
 
@@ -41,18 +41,21 @@ FEATURE_BACKWARD_DRIVING = OFF
 ################################
 # How long to stop at stop sign
 STOP_WAIT = 5
+EMERSON_CONSTANT = 0.4
 
 SPEED_MULTIPLIER = 1
-ANGLE_MULTIPLIER = 1
+ANGLE_MULTIPLIER = 0.85
 
 # THRESHOLD = 0.5
-STOP_THRESHOLD = 0.75 # if any lidar beam returns a distance less than this amount, STOP
-HALLWAY_THRESHOLD = 2.25 # makes sure both lidar_E and lidar_W are under this distance for hallway state
+STOP_THRESHOLD = 0.6 # if any lidar beam returns a distance less than this amount, STOP
+HALLWAY_THRESHOLD = 3.25 # makes sure both lidar_E and lidar_W are under this distance for hallway state
 HALLWAY_END_THRESHOLD = 3.0 # drive while staying centered until lidar_N reaches this distance
-TURN_THRESHOLD = 0.95 * HALLWAY_END_THRESHOLD # when the lidar_N see's this distance, turn right
+TURN_THRESHOLD = 1.15 * HALLWAY_END_THRESHOLD # when the lidar_N see's this distance, turn right
 # the turning threshold is slightly lower than the hallway's to let the robot drive forward a bit before turning
 
-THROTTLE = 0.65 * SPEED_MULTIPLIER
+THROTTLE = 0.75 * SPEED_MULTIPLIER
+if FEATURE_BACKWARD_DRIVING == ON:
+    THROTTLE = 0.6 * THROTTLE
 
 class LidarSubscriber(Node):
 
@@ -107,22 +110,23 @@ class LidarSubscriber(Node):
 
         dangerously_close = any(scan < STOP_THRESHOLD for scan in self.all_compass_scans)
 
-        # if the robot is too close to any object, send a stop message to the motors and set stopped attribute to 1
-        if dangerously_close:
-            # self.get_logger().info("WARNING: Dangerously close!")
-            self.current_state = "DANGER!"
-            self.motor_pub.stop_motor()
-            # self.stopped = 1
-
         # normal driving / correcting
         if self.reverse_drive == 0:
+            # if the robot is too close to any object, send a stop message to the motors and set stopped attribute to 1
+            if dangerously_close:
+                # self.get_logger().info("WARNING: Dangerously close!")
+                self.current_state = "DANGER!"
+                self.motor_pub.start_motor_forward(0.85*THROTTLE)
+            # self.stopped = 1
+
+
             # state when the robot is approaching a wall directly in front of it
             # in this case, turn the robot right until lidar_N goes back to state 1 (hopefully)
             # this doesn't check that the right is another hallway
             # if going down a corridor with no turns, then this behavior will cause it to u-turn or stop depending on its speed
-            if lidar_N <= TURN_THRESHOLD and ((lidar_NE > lidar_N) or (lidar_E > lidar_N)):
-                self.motor_pub.current_throttle = 0.8 * THROTTLE
-                self.motor_pub.set_angle(-0.6)
+            elif lidar_N <= TURN_THRESHOLD and ((lidar_NE > lidar_N) or (lidar_E > HALLWAY_THRESHOLD)):
+                self.motor_pub.current_throttle = 1.0 * THROTTLE
+                self.motor_pub.set_angle(-0.8)
                 self.get_logger().info("found turn condition. turning right")
                 self.current_state = "Right Turn"
 
@@ -131,15 +135,15 @@ class LidarSubscriber(Node):
             elif lidar_N >= HALLWAY_END_THRESHOLD and lidar_E <= HALLWAY_THRESHOLD and lidar_W <= HALLWAY_THRESHOLD:
                 self.motor_pub.current_throttle = THROTTLE
                 # if the robot is closer to right wall, turn slightly left
-                if lidar_E < lidar_W:
+                if lidar_E - EMERSON_CONSTANT < lidar_W + EMERSON_CONSTANT:
                     # publish message
-                    self.motor_pub.set_angle(0.2 * ANGLE_MULTIPLIER)
+                    self.motor_pub.set_angle(0.25 * ANGLE_MULTIPLIER)
                     # self.get_logger().info("turning slightly left")
                     self.current_state = "Slight Left"
                 # if the robot is closer to left wall, turn slightly right
-                elif lidar_E > lidar_W:
+                elif lidar_E - EMERSON_CONSTANT > lidar_W + EMERSON_CONSTANT:
                     # publish message
-                    self.motor_pub.set_angle(-0.2 * ANGLE_MULTIPLIER)
+                    self.motor_pub.set_angle(-0.15 * ANGLE_MULTIPLIER)
                     # self.get_logger().info("turning slightly right")
                     self.current_state = "Slight Right"
 
@@ -148,40 +152,50 @@ class LidarSubscriber(Node):
                 self.motor_pub.start_motor_forward()
                 # self.get_logger().info("no other states active. driving forward")
                 self.current_state = "Forward"
+
+
         # reverse driving is the same as forward, but with different lidar beams
         else:
+            # if the robot is too close to any object, send a stop message to the motors and set stopped attribute to 1
+            if dangerously_close:
+                # self.get_logger().info("WARNING: Dangerously close!")
+                self.current_state = "DANGER!"
+                self.motor_pub.start_motor_forward(0.85*-THROTTLE)
+            # self.stopped = 1
+                
+                
             # state when the robot is approaching a wall directly in front of it
             # in this case, turn the robot right until lidar_N goes back to state 1 (hopefully)
             # this doesn't check that the right is another hallway
             # if going down a corridor with no turns, then this behavior will cause it to u-turn or stop depending on its speed
-            if lidar_S <= TURN_THRESHOLD and ((lidar_SW > lidar_S) or (lidar_W > lidar_S)):
-                self.motor_pub.current_throttle = 0.8 * -THROTTLE
-                self.motor_pub.set_angle(0.6)
+            if lidar_S <= TURN_THRESHOLD and ((lidar_SW > lidar_S) or (lidar_W > HALLWAY_THRESHOLD)):
+                self.motor_pub.current_throttle = 1.0 * -THROTTLE
+                self.motor_pub.set_angle(0.4)
                 self.get_logger().info("found turn condition. turning right")
-                self.current_state = "Right Turn"
+                self.current_state = "Reverse Right Turn"
 
             # state when the robot is in the middle of a hallway with no turns coming up and walls on either side
             # in this case, try and keep the robot centered by making small turns depending on proximity to walls
             elif lidar_S >= HALLWAY_END_THRESHOLD and lidar_E <= HALLWAY_THRESHOLD and lidar_W <= HALLWAY_THRESHOLD:
                 self.motor_pub.current_throttle = -THROTTLE
                 # if the robot is closer to right wall, turn slightly left
-                if lidar_E < lidar_W:
+                if lidar_E + EMERSON_CONSTANT > lidar_W - EMERSON_CONSTANT:
                     # publish message
-                    self.motor_pub.set_angle(-0.2 * ANGLE_MULTIPLIER)
+                    self.motor_pub.set_angle(-0.25 * ANGLE_MULTIPLIER)
                     # self.get_logger().info("turning slightly left")
-                    self.current_state = "Slight Left"
+                    self.current_state = "Reverse Slight Left"
                 # if the robot is closer to left wall, turn slightly right
-                elif lidar_E > lidar_W:
+                elif lidar_E + EMERSON_CONSTANT < lidar_W - EMERSON_CONSTANT:
                     # publish message
-                    self.motor_pub.set_angle(0.2 * ANGLE_MULTIPLIER)
+                    self.motor_pub.set_angle(0.125 * ANGLE_MULTIPLIER)
                     # self.get_logger().info("turning slightly right")
-                    self.current_state = "Slight Right"
+                    self.current_state = "Reverse Slight Right"
 
             # if none of the other states are active, simply drive forward
             else:
-                self.motor_pub.start_motor_forward()
+                self.motor_pub.start_motor_backward()
                 # self.get_logger().info("no other states active. driving forward")
-                self.current_state = "Forward"
+                self.current_state = "Reverse"
 
 class CameraSubscriber(Node):
     """ Recieves messages from camera """
@@ -192,40 +206,35 @@ class CameraSubscriber(Node):
         self.subscription  # prevent unused variable warning
         self.num_color = 0
         self.motor : MotorPublisher = motor_publisher
+        self.has_stopped = 0
         self.current_state = "None"
 
     def listener_callback(self, msg : Image):
-        if FEATURE_STOP_SIGN == ON:
-            # go through the camera message and filter on red pixels
-            # data is [b, g, r,   b, g, r,   b, g, r, ...]
-            BLUE = 0
-            GREEN = 1
-            RED = 2
-            # slice is [start:end:step], so doing [0::3] will give us every 3rd element
-            # green_data = msg.data[GREEN::3]
-            self.num_color = 0
-            i = 0
-            while i < len(msg.data):
-                # get b, g, r values
-                b = msg.data[i]
-                g = msg.data[i+1]
-                r = msg.data[i+2]
-                # method to recognise green
-                if (g / (b+r+1)) > 1:
-                    self.num_color+=1
-                    # print("b %d, g %d, r%d " % (b, g, r))
-                i+=3
-            if self.num_color > 2000:
-                self.get_logger().info("met threshold for red values %d " % self.num_color)
-                self.motor.stop_motor()
-                self.current_state = "Stopped"
-                self.get_logger().info("WARNING: Dangerously close!")
-                self.get_logger().info("Waiting... ")
-                for t in range(STOP_WAIT):
-                    self.get_logger().info("%d..." % (STOP_WAIT - t))
-                    time.sleep(1)
-                self.get_logger().info("Done Waiting")
-                self.motor.start_motor_forward()
+        # go through the camera message and filter on red pixels
+        # data is [b, g, r,   b, g, r,   b, g, r, ...]
+        self.num_color = 0
+        i = 0
+        while i < len(msg.data):
+            # get b, g, r values
+            b = msg.data[i]
+            g = msg.data[i+1]
+            r = msg.data[i+2]
+            # method to recognise green
+            if (g / (b+r+1)) > 1:
+                self.num_color+=1
+                # print("b %d, g %d, r%d " % (b, g, r))
+            i+=3
+        if self.num_color > 500:
+            self.get_logger().info("green values %d " % self.num_color)
+            self.motor.stop_motor()
+            self.current_state = "Stopped"
+            self.get_logger().info("Waiting... ")
+            for t in range(STOP_WAIT):
+                self.get_logger().info("%d..." % (STOP_WAIT - t))
+                time.sleep(1)
+            self.get_logger().info("Done Waiting")
+            self.motor.start_motor_forward()
+            self.has_stopped = 1
         return
 
 class MotorPublisher(Node):
@@ -253,6 +262,7 @@ class MotorPublisher(Node):
     def start_motor_backward(self):
         # set shared variables
         self.current_throttle = -THROTTLE
+        self.current_angle = 0.0
         self.publish_message()   
 
     def stop_motor(self):
@@ -317,19 +327,23 @@ def main(args=None):
         camera_subscriber = CameraSubscriber(motor_publisher)
 
     print("Spinning")
-    motor_publisher.start_motor_forward(1.0)
+    if FEATURE_BACKWARD_DRIVING == OFF:
+        motor_publisher.start_motor_forward(1.0)
+    else:
+        motor_publisher.start_motor_forward(-1.0)
 
     while(1):
         rclpy.spin_once(lidar_subscriber)
         if lidar_subscriber.stopped == 1:
             print("stop condition met")
             break
-        if count % 5 == 0:
+        if FEATURE_STOP_SIGN == ON:
+            if camera_subscriber.has_stopped == 0:
+                rclpy.spin_once(camera_subscriber)
+        if count % 2 == 0:
+            count = 0
             if FEATURE_TELEMETRY == ON:
                 publish_telemetry(motor_publisher, lidar_subscriber, camera_subscriber)
-        if FEATURE_STOP_SIGN == ON and count % 50 == 0:
-            count = 0
-            rclpy.spin_once(camera_subscriber)
 
         count+=1
 
